2a0b9ed77121e469cf1902321c7ec0f20d114df4
erwan_lemonnier <erwan_lemonnier>
2008-05-08 06:57:45 +0000

    updated module dependencies

--- t/check_requirements.pm
+++ t/check_requirements.pm
@@ -1,9 +1,10 @@
 #
-#   $Id: check_requirements.pm,v 1.1 2007-03-30 08:38:50 erwan_lemonnier Exp $
+#   $Id: check_requirements.pm,v 1.2 2008-05-08 06:57:45 erwan_lemonnier Exp $
 #
 #   check that all required modules are available
 #
 
-eval "use Hook::WrapSub"; plan skip_all => "missing module 'Hook::WrapSub'" if ($@);
+eval "use accessors"; plan skip_all => "missing module 'accessors'" if ($@);
+eval "use Sub::Name"; plan skip_all => "missing module 'Sub::Name'" if ($@);
 
 1;
